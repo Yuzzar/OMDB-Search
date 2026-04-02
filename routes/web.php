8c7aa5842b70cab26705aad336e0d1db84d2e9ce
yuzzarmalik <yuzzarmalik1@gmail.com>
2026-04-02 15:42:49 +0700

    feat: Add .dockerignore file and refactor API and web routes for improved performance and caching

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,18 @@
 <?php
 
-Route::get('/', function () {
-    return redirect()->route('movies.index');
-});
+use Illuminate\Support\Facades\Route;
+
+// Use Route::redirect instead of a Closure for better performance and caching
+Route::redirect('/', '/movies')->middleware('auth.check');
 
 Route::get('/login', 'AuthController@showLogin')->name('login');
 Route::post('/login', 'AuthController@login')->name('login.submit');
 Route::post('/logout', 'AuthController@logout')->name('logout');
 
-Route::post('/locale', function (\Illuminate\Http\Request $request) {
-    $locale = $request->input('locale', 'en');
-    $available = config('app.available_locales', ['en']);
-
-    if (in_array($locale, $available)) {
-        session(['locale' => $locale]);
-        app()->setLocale($locale);
-    }
-
-    return back();
-})->name('locale.switch');
+// Moved locale logic to AuthController@switchLocale for route caching compatibility
+Route::post('/locale', 'AuthController@switchLocale')->name('locale.switch');
 
 Route::middleware('auth.check')->group(function () {
-
     Route::get('/movies', 'MovieController@index')->name('movies.index');
     Route::get('/movies/load-more', 'MovieController@loadMore')->name('movies.load-more');
     Route::get('/movies/{imdbId}', 'MovieController@detail')->name('movies.detail');
